51e43dcd78dea835c12a0687e32cfb1d03a32a66
Andrei Pozolotin <andrei.pozolotin@gmail.com>
2021-11-17 16:23:35 -0600

    boards

--- user/croxer/board.sh
+++ user/croxer/board.sh
@@ -9,7 +9,7 @@
 # note: skr/smoothie needs initial manual flash via sd-disk
 #
 
-set -e
+#set -e
 
 readonly this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 readonly base_dir="$( cd "$this_dir/../.." && pwd)"
@@ -38,4 +38,12 @@ cd "$base_dir"
 readonly MCU_1="skr-v-1.3"
 readonly DEV_1="/dev/serial/by-id/usb-Klipper_lpc1768_0D50FF1502942EAF8D89515BC62000F5-if00"
 
+readonly MCU_2="octopus-v-1.1"
+readonly DEV_2="/dev/serial/by-id/usb-Klipper_XY_NONE-if00"
+
+readonly MCU_3="spider-v-1.1"
+readonly DEV_3="/dev/serial/by-id/usb-Klipper_ZZ_NONE-if00"
+
 install_firmware ${MCU_1} ${DEV_1}
+install_firmware ${MCU_2} ${DEV_2}
+install_firmware ${MCU_3} ${DEV_3}
