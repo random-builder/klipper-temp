4f0b8a381ecd24be6c00b4440f7d0409b22c8dab
Andrei Pozolotin <andrei.pozolotin@gmail.com>
2023-10-01 16:43:48 -0500

    update: rotation_distance, default_parameter

--- user/custom/config.sh
+++ user/custom/config.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+#
+# update klipper.bin on octopi
+#
+
 set -e
 
 octo_pi="pi@octopi1"
@@ -8,17 +12,28 @@ this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 #echo "### this_dir=$this_dir"
 
-echo "### config reset"
+echo "### service stop"
+ssh $octo_pi "sudo service klipper stop"
+
+echo "### build clean"
 ssh $octo_pi "cd /home/pi/klipper; make distclean"
 
 echo "### copy config.cfg"
-scp "$this_dir/config.cfg"  $octo_pi:/home/pi/klipper/.config
+scp "$this_dir/config.cfg"  "$octo_pi:/home/pi/klipper/.config"
 
-echo "### firmware rebuild"
-ssh $octo_pi "sudo service klipper stop"
-ssh $octo_pi "cd /home/pi/klipper; make flash FLASH_DEVICE=/dev/ttyACM0"
+# board mode = normal ::
+# Bus 001 Device 007: ID 1d50:614e OpenMoko, Inc. 
 
-echo "### service restart"
+# board mode = bootloader ::
+# Bus 001 Device 005: ID 1d50:6015 OpenMoko, Inc. Smoothieboard
+
+echo "### build reflash"
+#FLASH_DEVICE="1d50:6015"
+#FLASH_DEVICE="/dev/ttyACM0"
+FLASH_DEVICE="/dev/serial/by-id/usb-Klipper_lpc1768_16100006262006AF566E6E58C32000F5-if00"
+ssh $octo_pi "cd /home/pi/klipper; make flash FLASH_DEVICE=$FLASH_DEVICE"
+
+echo "### service start"
 ssh $octo_pi "sudo systemctl enable klipper"
 ssh $octo_pi "sudo service klipper stop"
 ssh $octo_pi "sudo rm -rf /tmp/klippy*.log*"
