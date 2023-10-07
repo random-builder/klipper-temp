190cf001e4017a80a3b79965e45755c9ff45c355
Andrei Pozolotin <andrei.pozolotin@gmail.com>
2023-10-07 11:33:01 -0500

    use make1

--- user/custom/config.sh
+++ user/custom/config.sh
@@ -1,28 +1,31 @@
 #!/usr/bin/env bash
 
 #
-# update klipper.bin on octopi
+# update klipper.bin on make1
 #
 
 set -e
 
-octo_pi="pi@octopi1"
+make_host="make_print@make1"
+klipper_unit="make_klipper-wanhao_d6.service"
+klipper_data="/home/make_print/wanhao_d6/klip/data"
+klipper_repo="/home/make_print/wanhao_d6/klip/repo"
 
 this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 #echo "### this_dir=$this_dir"
 
 echo "### service stop"
-ssh $octo_pi "sudo service klipper stop"
+ssh $make_host "sudo systemctl stop $klipper_unit"
 
 echo "### build clean"
-ssh $octo_pi "cd /home/pi/klipper; make distclean"
+ssh $make_host "cd $klipper_repo; make distclean"
 
 echo "### copy config.cfg"
-scp "$this_dir/config.cfg"  "$octo_pi:/home/pi/klipper/.config"
+scp "$this_dir/config.cfg"  "$make_host:$klipper_repo/.config"
 
 # board mode = normal ::
-# Bus 001 Device 007: ID 1d50:614e OpenMoko, Inc. 
+# Bus 001 Device 007: ID 1d50:614e OpenMoko, Inc. lpc1768
 
 # board mode = bootloader ::
 # Bus 001 Device 005: ID 1d50:6015 OpenMoko, Inc. Smoothieboard
@@ -31,10 +34,12 @@ echo "### build reflash"
 #FLASH_DEVICE="1d50:6015"
 #FLASH_DEVICE="/dev/ttyACM0"
 FLASH_DEVICE="/dev/serial/by-id/usb-Klipper_lpc1768_16100006262006AF566E6E58C32000F5-if00"
-ssh $octo_pi "cd /home/pi/klipper; make flash FLASH_DEVICE=$FLASH_DEVICE"
+ssh $make_host "cd $klipper_repo ; make flash FLASH_DEVICE=$FLASH_DEVICE"
+
+exit
 
 echo "### service start"
-ssh $octo_pi "sudo systemctl enable klipper"
-ssh $octo_pi "sudo service klipper stop"
-ssh $octo_pi "sudo rm -rf /tmp/klippy*.log*"
-ssh $octo_pi "sudo service klipper start"
+ssh $make_host "sudo systemctl enable $klipper_unit"
+ssh $make_host "sudo systemctl stop   $klipper_unit"
+ssh $make_host "sudo rm -rf $klipper_data/klippy*.log*"
+ssh $make_host "sudo systemctl start  $klipper_unit"
--- user/custom/printer.sh
+++ user/custom/printer.sh
@@ -6,15 +6,19 @@
 
 set -e
 
-octo_pi="pi@octopi1"
+make_host="make_print@make1"
+klipper_unit="make_klipper-wanhao_d6.service"
+klipper_data="/home/make_print/wanhao_d6/klip/data"
+klipper_repo="/home/make_print/wanhao_d6/klip/repo"
+
 this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 #echo "### this_dir=$this_dir"
 
 echo "### copy printer.cfg"
-scp "$this_dir/printer.cfg" $octo_pi:/home/pi/printer.cfg
+scp "$this_dir/printer.cfg" $make_host:$klipper_repo/user/custom/printer.cfg
 
-echo "### service restart"
-ssh $octo_pi "sudo service klipper stop"
-ssh $octo_pi "sudo rm -rf /tmp/klippy*.log*"
-ssh $octo_pi "sudo service klipper start"
+echo "### service unit restart"
+ssh $make_host "sudo systemctl stop   $klipper_unit"
+ssh $make_host "rm -rf $klipper_data/service.log"
+ssh $make_host "sudo systemctl start  $klipper_unit"
